c53380a9f02d49318678622b3141eef99a0d6d3b
Ulrich Hecht <ulrich.hecht@gmail.com>
2015-03-12 20:28:04 +0100

    input.f: translate "waitkey" to Thumb
    This completes the conversion of input.f to Thumb.
    
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- optional/input.f
+++ optional/input.f
@@ -38,22 +38,21 @@ code-thumb keys ( -- n )
 end-code
 
 \ wait for button in mask to be pressed and released
-code waitkey ( n1 -- n2 )
-	REGISTERS ## v1 mov,
-	$130 ## v1 v1 add,
+code-thumb waitkey ( n1 -- n2 )
+	$4000130 v1 LITERAL
 	
 	\ wait for any button in the mask
 	l: __press
 	v1 0@ v2 ldrh,
-	v2 tos v2 and,
-	v2 tos v2 s! eor,
+	tos v2 and,
+	tos v2 eor,	\ eors, actually
 	__press eq? b,
 	
 	\ wait for the same button to be released
 	l: __depress
-	v1 0@ v3 ldrh,
-	v3 v2 v3 and,
-	v2 v3 cmp,
+	v1 0@ v0 ldrh,
+	v2 v0 and,
+	v2 v0 cmp,
 	__depress ne? b,
 	
 	\ done
