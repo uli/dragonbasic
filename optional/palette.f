a176a7da2c287d3ba01ead4f74f2ea4b3fa910f1
Ulrich Hecht <ulrich.hecht@gmail.com>
2015-04-03 15:36:56 +0200

    palette.f: inline "rgb{r,g,b}"
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- optional/palette.f
+++ optional/palette.f
@@ -14,9 +14,9 @@
 	10 # n* swap 5 # n* + + ;
 
 \ components of a color
-:n rgbr ( color -- r ) $1f # and ;
-:n rgbg ( color -- g ) 5 # n/ $1f # and ;
-:n rgbb ( color -- g ) 10 # n/ $1f # and ;
+:i rgbr ( color -- r ) $1f # and ;
+:i rgbg ( color -- g ) 5 # n/ $1f # and ;
+:i rgbb ( color -- g ) 10 # n/ $1f # and ;
 
 \ palette constant data
 label __colors
