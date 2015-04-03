ae134fc5a9dc128f6a2cbfe45174d1b4a6033d1b
Ulrich Hecht <ulrich.hecht@gmail.com>
2015-04-03 15:38:29 +0200

    sram.f: /8: inline
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- optional/sram.f
+++ optional/sram.f
@@ -2,7 +2,7 @@
   -- Original code by Jeff Massung, 2003 }
 
 \ shift masking
-:n /8 ( n1 -- n1 n2 ) dup 8 # n/ ;
+:i /8 ( n1 -- n1 n2 ) dup 8 # n/ ;
 :n *8 ( n1 n2 -- n3 ) swap 8 # n* + ;
 
 \ save data to static ram
