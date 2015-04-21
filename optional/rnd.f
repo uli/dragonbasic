4e23b3970c7729a9ced5556c5c62448c711ad439
Ulrich Hecht <ulrich.hecht@gmail.com>
2015-04-21 14:03:21 +0200

    rnd: translate RND to BASIC
    Slightly faster.
    
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- optional/rnd.f
+++ optional/rnd.f
@@ -4,10 +4,6 @@
 \ set the seed
 :n seed ( x -- ) __rnd_seed ! ;
 
-\ return a random number from 0-7FFF
-:n rnd ( -- n ) 
-	__rnd_seed @ 69069 # * 1+ dup __rnd_seed ! 17 # n/ ;
-
 \ return a rando, number between min and max-1
 : random ( min max -- n )
 	\ rnd-(max-min)*(rnd/(max-min))+min
