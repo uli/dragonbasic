8e359d74c824ee311df82215af0b6f0a902f5662
Ulrich Hecht <ulrich.hecht@gmail.com>
2015-04-21 13:59:35 +0200

    rnd: rename (seed) to __rnd_seed and define in BASIC
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- optional/rnd.f
+++ optional/rnd.f
@@ -1,14 +1,12 @@
 { -- MF/TIN random number generator
   -- Copyright (c) 2003 by Jeff Massung }
 
-variable (seed)
-
 \ set the seed
-:n seed ( x -- ) (seed) ! ;
+:n seed ( x -- ) __rnd_seed ! ;
 
 \ return a random number from 0-7FFF
 :n rnd ( -- n ) 
-	(seed) @ 69069 # * 1+ dup (seed) ! 17 # n/ ;
+	__rnd_seed @ 69069 # * 1+ dup __rnd_seed ! 17 # n/ ;
 
 \ return a rando, number between min and max-1
 : random ( min max -- n )
