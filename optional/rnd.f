b732db940597ef1232a35b7c9961bcbe2dc7f0e4
Ulrich Hecht <ulrich.hecht@gmail.com>
2015-04-21 16:54:34 +0200

    rnd: translate "random" to BASIC
    Two bytes larger, but slightly faster.  (The dominant factor is the
    division, though, and that does not change.)
    
    Signed-off-by: Ulrich Hecht <ulrich.hecht@gmail.com>

--- optional/rnd.f
+++ optional/rnd.f
@@ -3,8 +3,3 @@
 
 \ set the seed
 :n seed ( x -- ) __rnd_seed ! ;
-
-\ return a rando, number between min and max-1
-: random ( min max -- n )
-	\ rnd-(max-min)*(rnd/(max-min))+min
-	over - rnd dup a! swap dup 7 swi * - + ;
